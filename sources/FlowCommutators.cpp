62ffe60e9de58628350c5e50f34fb2947e7bc63b
Joshua <joshua.althueser@gmail.com>
2026-07-18 20:25:35 +0200

    Updated CMake

--- sources/FlowCommutators.cpp
+++ sources/FlowCommutators.cpp
@@ -1,16 +1,16 @@
-#include "coulomb_transform.hpp"
-#include "phonon_higher_order.hpp"
-#include "phonon_first_order.hpp"
 #include "commute_eta_eta.hpp"
+#include "coulomb_transform.hpp"
+#include "phonon_first_order.hpp"
+#include "phonon_higher_order.hpp"
 
 using namespace mrock::symbolic_operators;
 
 int main(int argc, char** argv) {
-	// When running, comment out everything but the function you need.
-	// By default everything is listed so that compilation of all variants can be tested.
-	phonon_first_order();
-	phonon_higher_order();
-	coulomb_transform();
-	commute_eta_eta();
-	return 0;
+    // When running, comment out everything but the function you need.
+    // By default everything is listed so that compilation of all variants can be tested.
+    phonon_first_order();
+    phonon_higher_order();
+    coulomb_transform();
+    commute_eta_eta();
+    return 0;
 }
